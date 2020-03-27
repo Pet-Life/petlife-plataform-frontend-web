f83545abd38bbfc56ddea34aa006f1b375505c22
DouglasPimentel <dev.pimentel@gmail.com>
2020-03-26 23:53:23 -0300

    Adicionado a propriedade alt no componente Logo

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -8,7 +8,7 @@ const Logo = () => {
   return (
     <LogoWrapper>
       <Link href='/'>
-        <LogoImage src={logo} />
+        <LogoImage src={logo} alt="Logo Petlife"/>
       </Link>
     </LogoWrapper>
   );
