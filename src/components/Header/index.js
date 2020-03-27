44e8d1dd3fc9d8b4a38856f901c8c5e6b4094a51
DouglasPimentel <dev.pimentel@gmail.com>
2020-03-26 22:34:06 -0300

    Criado componente Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import { HeaderWrapper, Title } from './styles';
+
 function Header() {
-  return
+  return (
+    <HeaderWrapper class="header-wrapper">
+      <Title>Home</Title>
+    </HeaderWrapper>
+  );
 }
 
 export default Header;
