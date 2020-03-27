8f1056d731d2d6aa5f67551bbf0f1474a5b0bd48
DouglasPimentel <dev.pimentel@gmail.com>
2020-03-26 23:33:10 -0300

    Criado componente Button

---
+++ src/components/Button/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { ButtonStyle } from './styles';
+
+const Button = () => {
+  return (
+    <>
+      <ButtonStyle>
+        Entrar
+      </ButtonStyle>
+    </>
+  );
+}
+
+export default Button;
---
+++ src/components/Button/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const ButtonStyle = styled.button`
+  width: 150px;
+  height: 48px;
+  background: ${props => props.primary ? "#219653" : "#FFF"};
+  color: ${props => props.primary ? "#FFF" : "#219653"};
+  font-size: 1.125em;
+  border: 2px solid #219653;
+  border-radius: 4px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: background 0.2s, color 0.2s;
+
+  &:hover {
+    background: ${props => props.primary ? "#FFF" : "#219653"};
+    color: ${props => props.primary ? "#219653" : "#FFF"};    
+  }
+`;
