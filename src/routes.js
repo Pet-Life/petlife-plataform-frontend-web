1a417659c597dc2fbe5d99b3036c31d05d652fe2
DouglasPimentel <dev.pimentel@gmail.com>
2020-04-15 12:17:13 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,6 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Search from "./pages/Search";
 
 const Routes = () => {
   return (
@@ -14,7 +13,6 @@ const Routes = () => {
         <Route path="/" exact component={Home} />
         <Route path="/entrar" component={Login} />
         <Route path="/cadastro" component={Register} />
-        <Route path="/buscar" component={Search} />
       </Switch>
     </BrowserRouter>
   );
