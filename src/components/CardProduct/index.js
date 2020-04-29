0d830f5f439adf250b5e1d0f108b6adb29a32f51
DouglasPimentel <dev.pimentel@gmail.com>
2020-04-28 22:19:16 -0300

    ajustado card produtos

--- src/components/CardProduct/index.js
+++ src/components/CardProduct/index.js
@@ -6,11 +6,20 @@ import img from "../../assets/racao_1.jpg";
 
 const CardProduct = () => {
   return (
-    <S.CardWrapper>
-      <S.Photo src={img} />
-      <S.CardTitle>Ração Special Dog Prime para Cães Adultos</S.CardTitle>
-      <S.Span>R$ 174,00</S.Span>
-    </S.CardWrapper>
+    <S.Link href="#">
+      <S.CardWrapper>
+        <S.Photo src={img} />
+        <S.StartWrapper>
+          <S.StartIcon />
+          <S.StartIcon />
+          <S.StartIcon />
+          <S.StartIcon />
+          <S.StartIcon />
+        </S.StartWrapper>
+        <S.CardTitle>Ração Special Dog Prime para Cães Adultos</S.CardTitle>
+        <S.Span>R$ 174,00</S.Span>
+      </S.CardWrapper>
+    </S.Link>
   );
 };
 
