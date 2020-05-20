deeddb79f75d52536993af30a484db20a2e5cd30
DouglasPimentel <dev.pimentel@gmail.com>
2020-05-20 13:12:33 -0300

    Editado modal para buscar petshop

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
+import api from "../../services/api";
 
 import * as S from "./styled";
 
 import Logo from "../../components/Logo";
 
-const Register = () => {
+const Register = ({ history }) => {
   const [name, setName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name && !lastName && !email && !password) {
+      setErrors({ error: "os campos não podem está vazio." });
+    } else if (!name) {
+      setErrors({ error: "o campo nome não podem está vazio." });
+    } else if (!lastName) {
+      setErrors({ error: "o campo sobrenome não podem está vazio." });
+    } else if (!email) {
+      setErrors({ error: "o campo email não podem está vazio." });
+    } else if (!password) {
+      setErrors({ error: "o campo senha não podem está vazio." });
+    }
+  }
 
   return (
     <>
@@ -16,34 +34,45 @@ const Register = () => {
         <Logo />
       </S.HeaderWrapper>
       <S.RegisterContent>
-        <S.Form>
+        <S.Form onSubmit={handleSubmit}>
           <S.Title>Cadastra-se</S.Title>
+          {errors && <S.ErrorText>{errors.error}</S.ErrorText>}
           <S.Input
             type="text"
+            id="name"
+            name="name"
             placeholder="Nome"
             value={name}
             onChange={(event) => setName(event.target.value)}
           />
           <S.Input
             type="text"
+            id="lastname"
+            name="lastname"
             placeholder="Sobrenome"
             value={lastName}
             onChange={(event) => setLastName(event.target.value)}
           />
           <S.Input
             type="email"
+            id="email"
+            name="email"
             placeholder="E-mail"
             value={email}
             onChange={(event) => setEmail(event.target.value)}
           />
           <S.Input
             type="password"
+            id="password"
+            name="password"
             autoComplete="off"
             placeholder="Senha"
             value={password}
             onChange={(event) => setPassword(event.target.value)}
           />
-          <S.Button type="submit">Cadastrar</S.Button>
+          <S.Button type="submit" className="btn">
+            Cadastrar
+          </S.Button>
           <S.Link href="/entrar">Já tenho uma conta. Entrar</S.Link>
         </S.Form>
       </S.RegisterContent>
